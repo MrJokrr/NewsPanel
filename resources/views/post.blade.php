74c9fb21ca2bb0ad6fadb155ee9dbda8b9c76bac
mrJoker <mrmolljoker@gmail.com>
2021-11-11 08:28:13 +0200

    eshkereme

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
     <div class="p-5 mb-4 bg-light rounded-3">
-
-        <div class="container-fluid py-5">
+        <h2>{{ $post['name'] }}</h2>
+        <p class="text-muted">{{ $post['created_at'] }}</p>
+        <div class="container-fluid py-2">
             <img src="{{ $post['image'] }}" alt="" class="img-uploaded"
                  style="display: block; height: 400px">
-            {{ $post['name'] }}
-            <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+            <p class="col-md-8 fs-4">{{ $post['text'] }}</p>
         </div>
     </div>
 @endsection
